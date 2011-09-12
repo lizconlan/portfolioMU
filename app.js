85d88d40b24f7978f9c7bfc607dc2358bb88f5c4
lizconlan <lizconlan@gmail.com>
2011-09-12 12:12:57 +0100

    refactoring into a module file

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var app = module.exports = express.createServer();
 var util = require('util')
 var Step = require('step');
 
-//var scraperwiki = require('./lib/scraperwiki.js')
+var scraperwiki = require('./lib/scraperwiki.js')
 
 // Configuration
 
@@ -33,71 +33,14 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', function(req, res){
-  //sw = new scraperwiki('lizconlan');
-  function loadAll(username, callback) {
-    Step(
-      function () {
-        getData(username, this);
-      },
-      function (list) {
-        var repos = new Array();
-        var group = this.group();
-        list.forEach(function (repo) {
-          getDetail(repo, repos, group());
-        });
-      },
-      function (list) {
-        callback(list);
-      }
-    )
-  }
-  
-  loadAll('lizconlan', function(repos) {
+  result = new scraperwiki('lizconlan', function(sw) {
     res.render('stuff', {
-      user: 'lizconlan',
-      own: repos
-    });
-  });  
+          user: sw.username
+        , own: sw.own
+        });
+  });
 });
 
-
-
-function getData(username, callback) {
-  var list = new Array();
-  rest.get('https://api.scraperwiki.com/api/1.0/scraper/getuserinfo?format=jsondict&username=' + username ).on('complete', function(data) {
-    projects = data[0].coderoles.owner;
-    for (var i in projects) {
-      if (projects[i].indexOf(".emailer") < 0) {
-        list.push(projects[i]);
-      }
-    }
-    // for (var i in list) {
-    //   getDetail(list[i], function(repo_data) {
-    //     own.push(repo_data);
-    //   });
-    // }
-    callback(list);
-  });
-}
-
-function getDetail(repo, list, callback) {
-  var link = "https://scraperwiki.com/scrapers/" + repo;
-  rest.get("https://api.scraperwiki.com/api/1.0/scraper/getinfo?format=jsondict&name=" + repo + "&version=-1").on('complete', function(data) {
-    repo_data = {"sw_name": repo, "name": data[0].title, "html_url": link, "language": capLanguage(data[0].language), "description": data[0].description, "homepage": ""}
-    list.push(repo_data);
-    callback(list);
-  });
-}
-
-function capLanguage(text) {
-  if (text == "php") {
-    return "PHP"
-  } else {
-    first = text.charAt(0);
-    return first.toUpperCase() + text.substring(1);
-  }
-end
-}
 
 // start the server!
 var port = process.env.PORT || 3000;
